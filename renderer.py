32b7d9ac8196d43b63fc2e0663170936e5e96560
unknown <mhoff12358@gmail.com>
2013-11-11 11:20:04 -0600

    Shape sets can now be cycled, and fleshed out the list of mappings and shape sets. Also it has the nano key shape set, all is right with the world.

--- renderer.py
+++ renderer.py
@@ -26,13 +26,15 @@ def joukowsky(z):
 
 def addjoukowskyfoil(shapes, radius, theta):
 	shapes.append(Shape(start_t = 0, end_t = 2*math.pi, num_t = 100, pointGenerator=functools.partial(lambda x, r, t: cnumpolar(r, x)+cnumxy(1-r*math.cos(t), math.sin(t)), r=radius, t=theta)))
+	return shapes
 
 class Renderer(object):
-	def __init__(self, game):
+	def __init__(self, game, initMap = 0, initShapes = 0):
 		self.game = game
 
 		self.toDraw = {"axes":1, "shapes":1, "graph":1}
 
+		self.mapIndex = 0
 		self.mapOptions = [
 		{"name":"unitcircle", "maps":[], "shapes":[], "windowScalePre":1, "windowScalePost":1,
 		 "pcfKwargs":{"numRad":20, "minRad":0, "maxRad":1, "numTheta":100},
@@ -51,17 +53,51 @@ class Renderer(object):
 		 "mapKwargs":{"numRad":20, "minRad":1, "maxRad":5, "numTheta":100, "maxTheta":2*math.pi}},
 		]
 
-		self.assignMap(0)
+		x3grid = []
+		nhelp = math.acos(1/1.25)
+		nhelp2 = math.asin(1/1.25)
+
+		self.shapeIndex = 0
+		self.shapecycle = [[],
+		[Shape(start_t=0, end_t=9, num_t=100, pointGenerator=lambda x: cnumxy(x+1, 0)),
+			Shape(start_t=0, end_t=9, num_t=100, pointGenerator=lambda x: cnumxy(1, x), color=(1,1,1))],
+		[Shape(start_t=0, end_t=9, num_t=100, pointGenerator=lambda x: cnumxy(2, x)),
+			Shape(start_t=0, end_t=9, num_t=100, pointGenerator=lambda x: cnumxy(x, x), color=(1,1,1))],
+		addjoukowskyfoil([], 1.25, .2),
+		addjoukowskyfoil([], 1, 0),
+		x3grid,
+		[Shape(start_t=0, end_t=2*math.pi, num_t = 100, pointGenerator=lambda x:cnumpolar(.5, x)+cnumxy(0,1)),
+			Shape(start_t=-math.pi/2+nhelp2, end_t=3./2*math.pi-nhelp, num_t = 100, pointGenerator=lambda x:cnumpolar(1.25, x)+cnumxy(0,1)),
+			Shape(start_t=0, end_t=2*math.pi, num_t = 100, pointGenerator=lambda x:cnumpolar(.5, x)+cnumxy(0,-1)),
+			Shape(start_t=-3./2*math.pi+nhelp, end_t=math.pi/2.-nhelp2, num_t = 100, pointGenerator=lambda x:cnumpolar(1.25, x)+cnumxy(0,-1)),
+			Shape(start_t=1, end_t=2.25, num_t = 100, pointGenerator=lambda x:cnumxy(x, -.25)),
+			Shape(start_t=1, end_t=2.25, num_t = 100, pointGenerator=lambda x:cnumxy(x, .25)),
+			Shape(start_t=-.25, end_t=.25, num_t = 100, pointGenerator=lambda y:cnumxy(2.25, y)),],
+		]
+		for i in range(7):
+			x3grid.append(Shape(start_t=0, end_t=6, num_t=100, pointGenerator=functools.partial(lambda x, y: cnumxy(-3+x, y), y=i-3)))
+			x3grid.append(Shape(start_t=0, end_t=6, num_t=100, pointGenerator=functools.partial(lambda y, x: cnumxy(x, -3+y), x=i-3)))
+
+		self.assignMap(initMap)
+		self.assignShapes(initShapes)
+
+	def assignShapes(self, newIndex):
+		self.shapeIndex = newIndex % len(self.shapecycle)
+		self.shapes = self.shapecycle[self.shapeIndex]
+		print self.map["name"], self.shapeIndex
+
+	def incrementShapes(self):
+		self.assignShapes(self.shapeIndex+1)
+
+	def decrementShapes(self):
+		self.assignShapes(self.shapeIndex-1)
 
 	def assignMap(self, newIndex):
 		self.mapIndex = newIndex % len(self.mapOptions)
 		self.map = self.mapOptions[self.mapIndex]
 		self.startingPCF = PolarComplexField(**self.map["pcfKwargs"])
 		self.mappedPCF = PolarComplexMappedField(mapFns = self.map["maps"], **self.map["mapKwargs"])
-		# self.shapes = [Shape(start_t=0, end_t=9, num_t=100, pointGenerator=lambda x: cnumxy(x+1, 0)),
-		# Shape(start_t=0, end_t=9, num_t=100, pointGenerator=lambda x: cnumxy(1, x), color=(1,1,1))]
-		self.shapes = []
-		addjoukowskyfoil(self.shapes, 1.25, .2)
+		print self.map["name"], self.shapeIndex
 
 	def incrementMap(self):
 		self.assignMap(self.mapIndex+1)
